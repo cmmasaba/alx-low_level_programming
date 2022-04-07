094eb055e9bef8194dd93829491283d3aa667ad8
Collins Mmasaba <mmasaba3142@gmail.com>
2022-04-07 07:11:08 -0700

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,5 +7,6 @@ int main(void)
 {
 	char *msg = "\"Programming is like building a multilingual puzzle";
 	puts(msg);
+
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of a char :%lu\n", sizeof(char) "byte(s\n)");
-	printf("Size of an int :%lu\n", sizeof(int) "byte(s)\n");
-	printf("Size of a long int :%lu\n", sizeof(double) "byte(s)\n");
-	printf("Size of a long long int :%lu\n", sizeof(double) "byte(s)\n");
-	printf("Size of a float :%lu\n", sizeof(float) "byte(s)\n");
+	printf("Size of a char :%lu byte(s)\n", sizeof(char));
+	printf("Size of an int :%lu byte(s)\n", sizeof(int));
+	printf("Size of a long int :%lu byte(s)\n", sizeof(double));
+	printf("Size of a long long int :%lu byte(s)\n", sizeof(double));
+	printf("Size of a float :%lu byte(s)\n", sizeof(float));
 	return (0);
 }
