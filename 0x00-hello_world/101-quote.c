72e9443b1ef349e295eda260466e9b6f755b3dd7
Collins Mmasaba <mmasaba3142@gmail.com>
2022-04-07 11:59:43 -0700

    Intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - directs the message to the stderr
+ * Return: 1 if the operation is successful
+ */
+int main(void)
+{
+	fprintf(stderr,
+	 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
