c0a405a41e2badc920b45a4d391a8cd12f52dfbf
Collins Mmasaba <mmasaba3142@gmail.com>
2022-04-08 09:49:43 -0700

    Software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * ranging from 0 to 99
+ * should be printed in ascending order
+ * you should only use putchar to print the numbers
+ * Return: 0 for successful build
+ */
+
+int main(void)
+{
+int m, n, o, p, a1, a2;
+
+m = n = o = p = 48;
+while (m < 58)
+{
+	n = 48;
+	while (n < 58)
+	{
+		o = 48;
+		while (o < 58)
+		{
+			p = 48;
+			while (p < 58)
+			{
+				a1 = (m * 10) + n;
+				a2 = (o * 10) + p;
+				if (a1 < a2)
+				{
+					putchar(m);
+					putchar(n);
+					putchar(' ');
+					putchar(o);
+					putchar(p);
+					if (m == 57 && n == 56 && o == 57 && p == 57)
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+				p++;
+			}
+			o++;
+		}
+		n++;
+	}
+	m++;
+}
+putchar('\n');
+return (0);
+}
