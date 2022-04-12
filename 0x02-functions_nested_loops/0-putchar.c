b6b815acce36f140bbae7316170d6b0b103fec64
Collins Mmasaba <mmasaba3142@gmail.com>
2022-04-12 04:17:07 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include  <unistd.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 for a succefull build
+ */
+
+int main(void)
+{
+	char p[8] = "_putchar";
+	int i = 0;
+
+	while (i <= 8)
+	{
+		_putchar(p[i]);
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+}
