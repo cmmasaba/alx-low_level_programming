f51487ac7f1fef132b1036f30a67725726688a10
Collins Mmasaba <85477468+cmmasaba@users.noreply.github.com>
2022-04-14 18:48:36 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,23 +13,25 @@ int main(void)
 {
 	int a;
 
-	for (a = 1; a <= 100; a++)
+	a = 1;
+	printf("%d ", a);
+	for (a = 2; a <= 100; a++)
 	{
 		if ((a % 3) == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if ((a % 5) == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if (((a % 3) == 0) && ((a % 5) == 0))
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else
 		{
-			printf("%d ", a);
+			printf(" %d", a);
 		}
 	}
 	printf("\n");
