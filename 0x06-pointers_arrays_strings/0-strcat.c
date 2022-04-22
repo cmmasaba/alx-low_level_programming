b4f88d237a2a65df4d91c06d78c4f20f31520fd1
Collins Mmasaba <mmasaba3142@gmail.com>
2022-04-22 09:23:45 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[b] = '\0';
+	return (dest);
+}
