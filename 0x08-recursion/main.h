317ba85de3f65f2668b47ec18475d99e92cebfe4
Collins Mmasaba <mmasaba3142@gmail.com>
2022-04-28 07:32:14 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _sqrt_recursion(int n);
+int operation(int n, int a);
+int is_prime_number(int n);
+int prime(unsigned int n, unsigned int a);
+int is_palindrome(char *s);
+int compare(char *s, int a1, int a2);
+int _strlen_recursion(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
