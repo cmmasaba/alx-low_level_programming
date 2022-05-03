d586faae618c090f9307b36f7a65c059c599d71d
Collins Mmasaba <mmasaba3142@gmail.com>
2022-05-03 12:55:21 +0000

    Silence is argument carried out by other means

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints number of arguements passed to it followed by new line
- * @argc - number of command line arguements
+ * @argc: number of command line arguements
  * @argv: array of pointers to the command line arguements
  *
  * Return: 0 Always successful
