f297446d7349ad77ea754a92e6bccaed1b6a9dee
Collins Mmasaba <mmasaba3142@gmail.com>
2022-05-03 13:12:03 +0000

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: number of command line arguements
+ * @argv: array of pointers to the command line arguements
+ *
+ * Return: 0 Always successful
+ */
+
+int main(argc, char **argv)
+{
+	int a, b sum = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; j++)
+		{
+			if (!isdigit(argv[a][b]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[a]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
