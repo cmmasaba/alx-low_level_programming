7a90ae2f94cc6d28a4f51be2a01c67594bb0b5f6
Collins Mmasaba <mmasaba3142@gmail.com>
2022-05-11 01:36:27 +0000

    Trust not one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * malloc_checked - allocates memory using malloc
@@ -14,6 +15,6 @@ void *malloc_checked(unsigned int b)
 	str = malloc(b);
 	if (str == NULL)
 		exit(98);
-	
+
 	return (str);
 }
