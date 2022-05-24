b33cb1b9f53f04ad51f36eff8f337112cff16c7c
Collins Mmasaba <mmasaba3142@gmail.com>
2022-05-24 19:51:06 +0000

    The Hare and The Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,8 +4,8 @@
  * first - prints a string before main function is executed
  */
 
-void first(void)
+void first(void) __attribute__ ((constructor))
 {
-	printf("You're beat! and yet, you must allow,");
-	printf("\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
