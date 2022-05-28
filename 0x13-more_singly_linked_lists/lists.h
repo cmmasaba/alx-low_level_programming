c9f75580c925f0f082160d6e593ac8e2f29d493f
Collins Mmasaba <mmasaba3142@gmail.com>
2022-05-28 04:14:09 +0000

    header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -11,7 +11,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
@@ -30,6 +30,7 @@ typedef struct listptr
 	void *ptr;
 	struct listptr *next;
 } listptr;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
